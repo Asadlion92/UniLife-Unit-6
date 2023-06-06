fedbca310b824bd3aa76a9cc72c8aecb3a5116f5
Jafar Asad <rafajdasa@gmail.com>
2023-06-05 23:29:19 -0400

    completed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Homepage from './pages/Homepage/Homepage'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-    <div className='App'>Hello React</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+import {MdOutlineHome} from 'react-icons/md'
+import {AiOutlineHeart} from 'react-icons/ai'
+import {SlEnvolope} from 'react-icons/sl'
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <div className="left-content">
+        <MdOutlineHome className='house' />
+        <h3>UniLife</h3>
+      </div>
+      <div className="right-content">
+        <div className="shortlist">
+          <AiOutlineHeart />
+          <p>Shortlist</p>
+        </div>
+        <div className="contact-us">
+          <SlEnvolope />
+          <p>Contact Us</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
