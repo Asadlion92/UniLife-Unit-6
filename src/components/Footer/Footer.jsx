e7cca062b8624c985b555209822d283c6d83aac0
Jafar Asad <rafajdasa@gmail.com>
2023-06-06 23:19:49 -0400

    completed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Footer.css'
+import {RiCopyrightLine} from 'react-icons/ri'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+        <div className="footer-left-content">
+            <p>About Us</p>
+            <p>Terms & Conditions</p>
+            <p>Privacy & Cookie Policies</p>
+        </div>
+        <div className="footer-right-content">
+            <p>2023</p>
+            <div className='copyright'>
+              <RiCopyrightLine />
+              <p>UniLife</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
