a042083bb11a6c7cb622b8d58f79aaf0a4777dfa
Jafar Asad <rafajdasa@gmail.com>
2023-07-15 06:01:03 -0400

    completed the cityDetails.css page

--- src/pages/CityDetails/CityDetails.jsx
+++ src/pages/CityDetails/CityDetails.jsx
@@ -4,6 +4,7 @@ import Slider from '../../components/Slider/Slider'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import PropertyCard from '../../components/PropertyCard/PropertyCard'
+import students from '../../assets/students-icon.png'
 
 function CityDetails() {
 
@@ -39,7 +40,19 @@ function CityDetails() {
         <div className='property-items-container'>
           {card.map((item, index) =><PropertyCard key={index} property={item} />)}
         </div>
-        
+      </div>
+      <div className='city-details-btm'>
+        <div className="city-details-btm-content">
+          <div className="city-details-btm-content-left">
+            <h1>{`Being a student in ${city.city_name}`}</h1>
+            <p>{`${city.city_name} is a lively and multicultural city with a large student population. It is quite a compact city, so it is easy to get around and has a community feel. ${city.city_name} is the perfect mix of city and town life and has something to offer to anyone who calls it home.`}</p>
+            <br />
+            <p>{`${city.city_name} is home to three universities, the University of ${city.city_name}, ${city.city_name} Trinity University and ${city.city_name} Beckett University`}</p>
+          </div>
+          <div className="city-details-btm-content-right">
+            <img src={students} />
+          </div>
+        </div>
       </div>
       
     </div>
