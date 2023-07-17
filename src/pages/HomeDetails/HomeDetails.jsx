78995d0a9ab4d2be43d33496920e6cd7e27dbbc0
Jafar Asad <rafajdasa@gmail.com>
2023-07-17 11:59:01 -0400

    was able to resolve api issues with images array

--- src/pages/HomeDetails/HomeDetails.jsx
+++ src/pages/HomeDetails/HomeDetails.jsx
@@ -7,25 +7,32 @@ function HomeDetails() {
 
     const {property_id} = useParams()
     const [details, setDetails] = useState('')
+    const [images, setImages] = useState([])
     //HAVING AN ISSUE USING ARRAYS. WHEN THE PAGE RELOADS, IT CRASHES
+    //Solution, the API call was an Object not an array. I created another state to just capture the images array. Also, it might be wise to create a component for images, the information on the right and the bedroom prices 
 
     useEffect(
         ()=>{
             axios.get(`https://unilife-server.herokuapp.com/properties/${property_id}`)
             .then(res => {
-                console.log(res.data)
+                console.log(res.data.images)
                 //store data in state
                 setDetails(res.data)
+                setImages(res.data.images)
             })
             .catch(err => console.log(err))
         }, []
-        )
+    )
+        
 
   return (
     <div className='home-details-container'>
         <div className="home-details-top-section">
             <div className="details-top-section-left-content">
-                {/* <img src={details.images[0]} /> */}
+                <img src={images[0]} />
+                <img src={images[1]} />
+                <img src={images[2]} />
+                <img src={images[3]} />
 
             </div>
             <div className="details-top-section-right-content"></div>
